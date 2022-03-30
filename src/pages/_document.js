ee2b03e3b53c8011911d81bc704d04479733b2a9
Roel Jorna <roel.jorna@gmail.com>
2022-03-30 12:51:42 +0200

    Move webfonts to _document file

---
+++ src/pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://use.typekit.net/ioi0tun.css"
+            rel="stylesheet"
+          />
+          <link
+            href="http://fonts.cdnfonts.com/css/avenir"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
