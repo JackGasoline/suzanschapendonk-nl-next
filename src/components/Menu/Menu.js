06409836f7227c87af51dc3609a9f9dc1cdc09cd
Roel Jorna <roel.jorna@ntr.nl>
2022-08-22 13:16:04 +0200

    Update menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,11 +5,13 @@ import { slide as Menubar } from 'react-burger-menu'
 const Menu = () => {
     return (
         <Menubar>
-            <Link href='/' passHref>Home</Link>
-            <Link href='/mijnwerk' passHref>Mijn werk</Link>
-            <Link href='/contact' passHref>Contact</Link>
-            <Link href='/prints' passHref>Prints</Link>
-            <Link href='/overmij' passHref>Over mij</Link>
+            <ul>
+            <li><Link href='/' passHref>Home</Link></li>
+            <li><Link href='/mijnwerk' passHref>Mijn werk</Link></li>
+            <li><Link href='/contact' passHref>Contact</Link></li>
+            <li><Link href='/prints' passHref>Prints</Link></li>
+            <li><Link href='/overmij' passHref>Over mij</Link></li>
+            </ul>
         </Menubar>
     )
 }
